06ef5a6b6a51d23cf2c245caf8e714c3c8d376cf
Saurabh Tewary <tewary.saurabh@gmail.com>
2019-06-01 16:04:11 +0530

    Final Commit after adding comments

--- GLSProj/views.py
+++ GLSProj/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 import json
 
+
 def index(request):
     """
     View function for home page of the site rendering index.html
     """
 
     # Parsing the guide json file and passing it as context variable
-    guide = 'GLSProj//guide.json'
+    guide = "GLSProj//guide.json"
     with open(guide) as json_file:
         data = json.load(json_file)
         json_string = json.dumps(data)
         print(json_string)
-    return render(request, 'index.html', {'guide': json_string})
+    return render(request, "index.html", {"guide": json_string})
